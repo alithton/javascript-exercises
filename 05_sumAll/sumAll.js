2880fb3fc7e483cf6085402d3a0ef79a2ba83ec3
Alastair Smith <alastairfsmith26@gmail.com>
2022-02-25 11:57:40 -0700

    Edited sumAll function to conform to specifications in test file

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
 
+    if (!isNonNegativeInteger(a) || !isNonNegativeInteger(b)) {
+        return 'ERROR';
+    }
+
+    const larger = a < b ? b : a;
+    const smaller = a < b ? a : b;
+
+    let sum = 0;
+
+    for (let i = smaller; i <= larger; i++) {
+        sum += i;
+    }
+    return sum;
 };
+
+function isNonNegativeInteger(x) {
+    return Number.isInteger(x) && x >= 0;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
