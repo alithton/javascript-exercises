3af22d2a257b57f077c3ea6f22c73b54dd709f1b
Alastair Smith <alastairfsmith26@gmail.com>
2022-02-28 14:36:31 -0700

    Add fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,26 @@
-const fibonacci = function() {
-
+/*
+const fibonacci = function(n) {
+    if (n < 1) return 'OOPS';
+    if (n == 1 || n == 2) return 1;
+    return fibonacci(n-1) + fibonacci(n-2);
 };
+*/
+
+const fibonacci = function(n) {
+    if (n < 1) return 'OOPS';
+    
+    return fibIter(1, 0, n)
+};
+
+function fibIter(a, b, count) {
+    if (count == 0) {
+        return b;
+    } else {
+        return fibIter(a + b, a, count - 1);
+    }
+}
+
+console.log(fibonacci(5));
 
 // Do not edit below this line
 module.exports = fibonacci;
