71e93ebed8d0fa18c983792ec1c876d2ab2674ac
Alastair Smith <alastairfsmith26@gmail.com>
2022-02-28 13:40:24 -0700

    Complete functions in calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, item) => sum + item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product, item) => product * item, 1);
 };
 
-const power = function() {
-	
+const power = function(a, exp) {
+	return a ** exp;
 };
 
-const factorial = function() {
-	
+/* Recursive version:
+const factorial = function(a) {
+  if (a === 0) return 1;
+  return a * factorial(a-1);	
 };
+*/
 
+// Iterative version:
+const factorial = function(a) {
+  if (a === 0) return 1;
+  let result = a;
+  for (let i = a - 1; i > 0; i--) {
+    result *= i;
+  }
+  return result;
+};
 // Do not edit below this line
 module.exports = {
   add,
