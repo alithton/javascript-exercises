a328255f145e6804aadeaa0b1294a404c801efc6
Alastair Smith <alastairfsmith26@gmail.com>
2022-02-28 14:01:34 -0700

    Complete palindromes exercises to specifications

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    // Strip out any non-letter characters
+    let tidyString = string.toLowerCase().replace(/[^a-z]/g, '');
+    const reversed = Array.from(tidyString).reverse().join('');
+    return tidyString === reversed;
 };
 
 // Do not edit below this line
